d468b4c94090d1ec43270a208b0bd1c751aee877
harryd0311 <dhiman.3@iitj.ac.in>
2023-02-22 16:35:24 +0530

    first

---
+++ level_five/learning_users/basic_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)          # instead of inheriting, we use this
+    # User class already has fields like username, lastname, email, etc.
+    #additional
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
